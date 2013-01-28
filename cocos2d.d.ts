f7c1be0c35dd3cdf4d8789a38dac101ffac56782
Ilya Rezvov <spy_forever@pochta.ru>
2013-01-28 20:37:28 +0400

    add raw Node description

--- cocos2d.d.ts
+++ cocos2d.d.ts
@@ -24,7 +24,7 @@ declare class BObject {
 }
 
 declare class BArray extends BObject {
-    new (array: any[]): BArray;
+    constructor(array: any[]);
 
     getArray(): any[];
     
@@ -65,5 +65,132 @@ interface PlistOptions {
 }
 
 declare class Plist extends BObject {
-    new (opts: PlistOptions): Plist;
+    constructor(opts: PlistOptions);
+}
+
+declare var util: {
+    beget(o: any): void;
+
+    callback(target: any, method: any): void;
+
+    copy(o: any): any;
+
+    domReady(): void;
+
+    each(arr: any[], f:any => any);
+
+    extend(target: any, ext: any): void;
+
+    isArray(arr: any): bool;
+
+    isDate(d: any): bool;
+
+    isRegExp(r: any): bool;
+
+    map(arr: any[], f:any => any): any[];
+
+    merge(first: any, ...other: any[]): any;
+
+    populateIndex(parent: any, modules: string): any;
+
+    ready(f: Function): void;
+}
+
+module cocos.actions {
+    
+    export class Action extends BObject {
+	public isDone: any;
+	public tag: any;
+	public target: cocos.nodes.Node;
+    }
+
+}
+
+module cocos.nodes {
+    export class Node extends BObject {
+	public anchorPoint: geometry.Point;
+	public anchorPointInPixels: geometry.Point;
+	public boundingBox: geometry.Rect;
+	public children: Node[];
+	public contentSize: geometry.Size;
+	public opactity: number;
+	public parent: Node;
+	public position: geometry.Point;
+	public rotation: number;
+	public scale: number;
+	public scaleX: number;
+	public scaleY: number;
+	public tag: any;
+	public visible: bool;
+	public visibleRect: geometry.Rect;
+	public worldBoundingBox: geometry.Rect;
+	public zOrder: number;
+
+	constructor();
+	
+	addChild(child: { child: Node; z?: number; tag?: string; }): Node;
+	
+	cleanup(): void;
+
+	convertToNodeSpace(worldPoint: geometry.Point): void;
+
+	detatchChild(opts: any): void;
+
+	draw(context: CanvasRenderingContext2D, rect: geometry.Rect): void;
+	draw(context: WebGLRenderingContext, rect: geometry.Rect): void;
+	
+	getAction(opts: any): cocos.actions.Action;
+
+	getChild(opts: any): Node;
+
+	nodeToParentTransform(): void;
+	nodeToWorldTransform(): void;
+	parentToNodeTransform(): void;
+	
+	pauseSchedulerAndActions(): void;
+
+	removeChild(opts: any): void;
+	removeChildren(opts: any): void;
+	reorderChild(opts: any): void;
+	resumeSchedulerAndActions(): void;
+	runAction(action: cocos.actions.Action): void;
+
+	schedule(method: {method: string; interval?: number;}): void;
+	schedule(method: {method: Function; interval?: number;}): void;
+
+	scheduleUpdate(opts: any): void;
+	stopAllActions(): void;
+	transform(context: any): void;
+	
+	unschedule(method: string): void;
+	unschedule(method: Function): void;
+	unscheduleAllSelectors(): void;
+
+	unscheduleSelector(selector: any): void;
+	visit(context: any, rect: any): void;
+	worldToNodeTransform(): void;
+    }
+}
+
+module geometry {
+    export class Point {
+	public x: number;
+	public y: number;
+
+	constructor(x: number, y: number);
+    }
+
+    export class Rect {
+	public origin: Point;
+	public size: Size;
+
+	constructor(x: number, y: number, w: number, h: number);
+    }
+
+    export class Size {
+	public height: number;
+	public weight: number;
+
+	constructor(w: number, h: number);
+    }
 }
